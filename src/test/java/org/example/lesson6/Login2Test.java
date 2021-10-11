edc02ba9831827bc64ab4c236faf941ef643dd07
2509net <81954996+2509net@users.noreply.github.com>
2021-10-11 15:32:45 +0300

    ДЗ к уроку 6

---
+++ src/test/java/org/example/lesson6/Login2Test.java
@@ -0,0 +1,43 @@
+package org.example.lesson6;
+
+import jdk.jfr.Description;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class Login2Test extends BaseTest {
+    @BeforeEach
+    void setupBrowser() {
+        webDriver.get("https://crm.geekbrains.space/");
+    }
+
+    @Test
+    @Description("Тест создания заявки на расход")
+    void loginTest() {
+        new LoginPage(webDriver)
+                .fillLogin("Applanatest1")
+                .fillPassword("Student2020!")
+                .submitLogin();
+
+        new MainPage(webDriver).navigationMenu.openNavigationSubMenuItems("Расходы");
+        new ExpensesSubMenu(webDriver)
+                .expensesButtonClick()
+                .createExpenseButtonClick();
+
+        new CreateExpensePage(webDriver)
+                .fillExpenseAppointment("test")
+                .selectBusinessUnit("Research & Development")
+                .selectExpenditure("01101 - ОС: вычислительная техника инфраструктуры")
+                .selectCurrency("Доллар США")
+                .fillSumPlanInput("100")
+                .selectPlannedDate("15")
+                .saveAndCloseButtonClick();
+
+        webDriverWait.until(ExpectedConditions.invisibilityOf(webDriver.findElement(By.xpath("//div[@class='loader-frame well']"))));
+        Assertions.assertTrue(webDriver.findElement(By.xpath("//*[.='Заявка на расход сохранена']")).isDisplayed());
+
+        webDriver.get("https://afisha.ru");
+    }
+
+}
